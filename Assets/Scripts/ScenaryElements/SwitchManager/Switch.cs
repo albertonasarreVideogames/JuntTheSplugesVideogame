54068619cfd4e84cdad2848100258f5116db958d
AlbertoNasarre-creator <albertonasarrecancer@gmail.com>
2025-01-22 16:09:52 +0100

    add glow to animation on switch

--- Assets/Scripts/ScenaryElements/SwitchManager/Switch.cs
+++ Assets/Scripts/ScenaryElements/SwitchManager/Switch.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Switch : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnSwitchUpAnimation()
+    {
+        spriteRenderer.material.SetInt("_EnableColor", 1);
+    }
+
+    public void OnSwitchDownAnimation()
+    {
+        spriteRenderer.material.SetInt("_EnableColor", 0);
     }
 }
