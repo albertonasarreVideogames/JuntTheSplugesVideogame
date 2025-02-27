2295129ea2c8b6bbc9230e0d3c63dd44c17d588e
AlbertoNasarre-creator <albertonasarrecancer@gmail.com>
2025-02-27 10:26:19 +0100

    add rewindstate class

---
+++ Assets/Scripts/States/MovementsManagerPlay.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ButtonPresed
+{
+    RIGHT,
+    LEFT,
+    UP,
+    DOWN,
+    CHANGEPLAYER
+}
+public class MovementsManagerPlay
+{
+    public List<ButtonPresed> buttonspressed = new List<ButtonPresed>();
+
+    private Vector2 moventRight = new Vector2(1, 0);
+    private Vector2 moventLeft = new Vector2(-1, 0);
+    private Vector2 moventUp = new Vector2(0, 1);
+    private Vector2 moventDown = new Vector2(0, -1);
+
+    public void executeComand(int i)
+    {
+        if (buttonspressed[i] == ButtonPresed.RIGHT)
+        {
+            GamingState.Instance.Simulatemovement(moventRight);
+        }
+        if (buttonspressed[i] == ButtonPresed.LEFT)
+        {
+            GamingState.Instance.Simulatemovement(moventLeft);
+        }
+        if (buttonspressed[i] == ButtonPresed.UP)
+        {
+            GamingState.Instance.Simulatemovement(moventUp);
+        }
+        if (buttonspressed[i] == ButtonPresed.DOWN)
+        {
+            GamingState.Instance.Simulatemovement(moventDown);
+        }
+        if (buttonspressed[i] == ButtonPresed.CHANGEPLAYER)
+        {
+            Player.changePlayerTypeToMove();
+        }
+    }
+
+    public void AddMovementRight(int times) {
+
+        for (int i = 0; i < times; i++)
+        {
+            buttonspressed.Add(ButtonPresed.RIGHT);
+        }
+    }
+
+    public void AddMovementLeft(int times)
+    {
+        for (int i = 0; i < times; i++)
+        {
+            buttonspressed.Add(ButtonPresed.LEFT);
+        }
+    }
+
+    public void AddMovementUp(int times)
+    {
+        for (int i = 0; i < times; i++)
+        {
+            buttonspressed.Add(ButtonPresed.UP);
+        }
+    }
+
+    public void AddMovementDown(int times)
+    {
+        for (int i = 0; i < times; i++)
+        {
+            buttonspressed.Add(ButtonPresed.DOWN);
+        }
+    }
+
+    public void AddMChangePlayer(int times)
+    {
+        for (int i = 0; i < times; i++)
+        {
+            buttonspressed.Add(ButtonPresed.CHANGEPLAYER);
+        }
+    }
+}
---
+++ Assets/Scripts/States/RewindState.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewindState : MonoBehaviour
+{
+    private static bool spacePressedThisFrame = false;
+    public static IEnumerator RunTest(
+    string sceneName,
+    MovementsManagerPlay movementsManager,
+    GameState expectedState,
+    float timeBetweenmovementsMultiplicator = 2f
+)
+    {
+        //yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+        yield return new WaitForSeconds(0.1f * timeBetweenmovementsMultiplicator);
+
+        // Iterar sobre los comandos de movimiento
+        for (int i = 0; i < movementsManager.buttonspressed.Count; i++)
+        {
+            if (GamingState.Instance != null)
+            {
+                Debug.Log("Esperando antes de pulsar espacio " + i);
+
+                // Espera a que se presione la tecla de espacio (pero evita múltiples presiones rápidas)
+                yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space) && !spacePressedThisFrame && GamingState.Instance.getIfPlayersStopMoving());
+
+                // Marcar que la tecla espacio ha sido presionada
+                spacePressedThisFrame = true;
+
+                Debug.Log("Ejecutado " + i);
+                movementsManager.executeComand(i);
+
+                // Espera hasta que se libere la tecla de espacio para evitar múltiples activaciones
+                yield return new WaitUntil(() => !Input.GetKey(KeyCode.Space));
+
+                // Marcar que la tecla espacio ha sido liberada
+                spacePressedThisFrame = false;
+            }
+            else
+            {
+                Debug.Log("No GamingState");
+            }
+        }
+
+        // Destruir los objetos persistentes
+        GameObject[] persistentObjects = GameObject.FindGameObjectsWithTag("Persistent");
+        foreach (GameObject obj in persistentObjects)
+        {
+            UnityEngine.Object.Destroy(obj);
+        }
+
+        // Esperar un poco más para asegurarse de que ha acabado
+        yield return new WaitForSeconds(1f);
+
+        // Verificar el estado final
+        //Assert.AreEqual(GameManager.Instance.State, expectedState);
+    }
+
+}
