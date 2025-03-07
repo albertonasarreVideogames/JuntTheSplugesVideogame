b403cd83ec2152460a7feee9758a02fae5b93a1f
AlbertoNasarre-creator <albertonasarrecancer@gmail.com>
2025-03-07 17:33:58 +0100

    add world5 level10

--- Assets/Tests/LevelsTest/World5Test.cs
+++ Assets/Tests/LevelsTest/World5Test.cs
@@ -571,5 +571,48 @@ namespace LevelTests
 
             yield return TestUtilities.RunTest("World5Level9", movementsManager, GameState.Victory, 10.0f);
         }
+
+        [UnityTest]
+        [Timeout(900000)]
+        public IEnumerator Level10()
+        {
+            MovementsManager movementsManager = new MovementsManager();
+            movementsManager.AddMovementLeft(8);
+            movementsManager.AddMChangePlayer(1);
+            movementsManager.AddMovementUp(1);
+            movementsManager.AddMovementRight(8);
+            movementsManager.AddMovementDown(1);
+            movementsManager.AddMovementLeft(6);
+            movementsManager.AddMChangePlayer(1);
+            movementsManager.AddMovementRight(6);
+            movementsManager.AddMovementDown(1);
+            movementsManager.AddMovementRight(2);
+            movementsManager.AddMovementUp(1);
+            movementsManager.AddMovementLeft(17);
+            movementsManager.AddMChangePlayer(1);
+            movementsManager.AddMovementRight(7);
+            movementsManager.AddMovementDown(1);
+            movementsManager.AddMovementLeft(6);
+            movementsManager.AddMovementUp(1);
+            movementsManager.AddMovementLeft(1);
+            movementsManager.AddMovementDown(1);
+            movementsManager.AddMChangePlayer(1);
+            movementsManager.AddMovementRight(23);
+            movementsManager.AddMovementUp(9);
+            movementsManager.AddMovementDown(2);
+            movementsManager.AddMovementLeft(7);
+            movementsManager.AddMovementRight(1);
+            movementsManager.AddMovementUp(3);
+            movementsManager.AddMovementRight(5);
+            movementsManager.AddMChangePlayer(1);
+            movementsManager.AddMovementRight(6);
+            movementsManager.AddMovementDown(1);
+            movementsManager.AddMovementRight(14);
+            movementsManager.AddMovementUp(11);
+            movementsManager.AddMChangePlayer(1);
+            movementsManager.AddMovementRight(1);
+
+            yield return TestUtilities.RunTest("World5Level10", movementsManager, GameState.Victory, 10.0f);
+        }
     }
     }
