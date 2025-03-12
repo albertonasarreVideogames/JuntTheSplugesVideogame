64d6a304ed51ce46ef94a5cedc4abcb186be8d18
AlbertoNasarre-creator <albertonasarrecancer@gmail.com>
2025-03-12 22:24:57 +0100

    Update TestUtilities.cs

--- Assets/Tests/TestUtilities.cs
+++ Assets/Tests/TestUtilities.cs
@@ -8,7 +8,7 @@ public static class TestUtilities
 {
     public static IEnumerator RunTest(string sceneName, MovementsManager movementsManager, GameState expectedState,float timeBetweenmovementsMultiplicator = 2f)
     {
-        bool manualTestActivated = true;
+        bool manualTestActivated = false;
 
         yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
         yield return new WaitForSeconds(0.1f * timeBetweenmovementsMultiplicator);
