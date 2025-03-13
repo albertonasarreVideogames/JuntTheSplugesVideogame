f01b52734e9d3c9cb7eb2edab70c995cf2e1d07a
AlbertoNasarre-creator <albertonasarrecancer@gmail.com>
2025-03-13 13:25:13 +0100

    add world7level3

--- Assets/Tests/LevelsTest/World7Test.cs
+++ Assets/Tests/LevelsTest/World7Test.cs
@@ -45,5 +45,23 @@ namespace LevelTests
             yield return TestUtilities.RunTest("World7Level2", movementsManager, GameState.Victory, 10.0f);
         }
 
+        [UnityTest]
+        [Timeout(90000)]
+        public IEnumerator Level3()
+        {
+            MovementsManager movementsManager = new MovementsManager();
+            movementsManager.AddMovementRight(1);
+            movementsManager.AddMovementUp(3);
+            movementsManager.AddMovementDown(1);
+            movementsManager.AddMovementRight(2);
+            movementsManager.AddMovementUp(2);
+            movementsManager.AddMovementRight(2);
+            movementsManager.AddMovementUp(3);
+            movementsManager.AddMovementRight(3);
+            movementsManager.AddMovementUp(2);
+
+            yield return TestUtilities.RunTest("World7Level3", movementsManager, GameState.Victory, 10.0f);
+        }
+
     }
 }
