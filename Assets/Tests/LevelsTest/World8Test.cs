223db95b346333411e2e6a6f00ec1f0a0b4ab92f
AlbertoNasarre-creator <albertonasarrecancer@gmail.com>
2025-03-15 17:32:59 +0100

    add world8level1

---
+++ Assets/Tests/LevelsTest/World8Test.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+namespace LevelTests
+{
+    public class World8Test
+    {
+        [UnityTest]
+        [Timeout(90000)]
+        public IEnumerator Level1()
+        {
+            MovementsManager movementsManager = new MovementsManager();
+            movementsManager.AddMovementDown(1);
+            movementsManager.AddMovementRight(5);
+            movementsManager.AddMovementLeft(2);
+            movementsManager.AddMovementUp(9);
+            movementsManager.AddMovementRight(5);
+            movementsManager.AddMovementDown(1);
+            movementsManager.AddMovementRight(2);
+            movementsManager.AddMovementUp(2);
+
+            yield return TestUtilities.RunTest("World8Level1", movementsManager, GameState.Victory, 10.0f);
+        }
+
+    }
+}
+
+
