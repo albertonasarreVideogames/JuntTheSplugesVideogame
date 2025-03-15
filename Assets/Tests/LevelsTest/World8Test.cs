39da4c294f6d8dc51258c2661023d5e1a708ddd4
AlbertoNasarre-creator <albertonasarrecancer@gmail.com>
2025-03-15 18:13:26 +0100

    finish world8test1

--- Assets/Tests/LevelsTest/World8Test.cs
+++ Assets/Tests/LevelsTest/World8Test.cs
@@ -8,18 +8,52 @@ namespace LevelTests
     public class World8Test
     {
         [UnityTest]
-        [Timeout(90000)]
+        [Timeout(900000)]
         public IEnumerator Level1()
         {
             MovementsManager movementsManager = new MovementsManager();
-            movementsManager.AddMovementDown(1);
+            movementsManager.AddMovementLeft(5);
+            movementsManager.AddMChangePlayer(2);
+            movementsManager.AddMovementRight(1);
+            movementsManager.AddMChangePlayer(1);
+            movementsManager.AddMovementLeft(1);
+            movementsManager.AddMChangePlayer(2);
             movementsManager.AddMovementRight(5);
+            movementsManager.AddMChangePlayer(2);
+            movementsManager.AddMovementLeft(5);
+            movementsManager.AddMChangePlayer(1);
+            movementsManager.AddMovementRight(1);
+            movementsManager.AddMChangePlayer(2);
+            movementsManager.AddMovementLeft(1);
+
+            movementsManager.AddMChangePlayer(1);
+            movementsManager.AddMovementRight(11);
+            movementsManager.AddMovementLeft(4);
+            movementsManager.AddMChangePlayer(1);
             movementsManager.AddMovementLeft(2);
-            movementsManager.AddMovementUp(9);
-            movementsManager.AddMovementRight(5);
-            movementsManager.AddMovementDown(1);
+            movementsManager.AddMChangePlayer(1);
+            movementsManager.AddMovementLeft(2);
+            movementsManager.AddMChangePlayer(1);
+            movementsManager.AddMovementRight(8);
+
+            movementsManager.AddMovementRight(7);
+            movementsManager.AddMovementDown(11);
+            movementsManager.AddMovementUp(6);
+            movementsManager.AddMovementLeft(22);
+            movementsManager.AddMovementUp(6);
+            movementsManager.AddMChangePlayer(1);
+            movementsManager.AddMovementDown(11);
             movementsManager.AddMovementRight(2);
-            movementsManager.AddMovementUp(2);
+            movementsManager.AddMovementLeft(2);
+            movementsManager.AddMovementUp(11);
+            movementsManager.AddMChangePlayer(1);
+            movementsManager.AddMovementDown(11);
+            movementsManager.AddMovementRight(2);
+            movementsManager.AddMovementLeft(2);
+            movementsManager.AddMovementUp(11);
+
+
+
 
             yield return TestUtilities.RunTest("World8Level1", movementsManager, GameState.Victory, 10.0f);
         }
