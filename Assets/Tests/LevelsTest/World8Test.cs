b893386537150532e374342a7418118d7be7147a
AlbertoNasarre-creator <albertonasarrecancer@gmail.com>
2025-03-16 12:38:16 +0100

    lworld3lvel8

--- Assets/Tests/LevelsTest/World8Test.cs
+++ Assets/Tests/LevelsTest/World8Test.cs
@@ -216,12 +216,37 @@ namespace LevelTests
         public IEnumerator Level3()
         {
             MovementsManager movementsManager = new MovementsManager();
+            movementsManager.AddMovementUp(3);
+            movementsManager.AddMovementRight(4);
+            movementsManager.AddMovementDown(1);
             movementsManager.AddMovementLeft(5);
-            movementsManager.AddMChangePlayer(2);
-            movementsManager.AddMovementRight(1);
-            movementsManager.AddMChangePlayer(1);
+            movementsManager.AddMovementDown(4);
+            movementsManager.AddMovementRight(8);
+            movementsManager.AddMovementDown(2);
+            movementsManager.AddMovementUp(5);
+            movementsManager.AddMovementDown(2);
             movementsManager.AddMovementLeft(1);
-            movementsManager.AddMChangePlayer(2);
+            movementsManager.AddMovementUp(2);
+            movementsManager.AddMovementDown(1);
+            movementsManager.AddMovementRight(1);
+            movementsManager.AddMovementUp(1);
+
+            movementsManager.AddMovementLeft(9);
+            movementsManager.AddMovementDown(4);
+            movementsManager.AddMovementRight(6);
+            movementsManager.AddMovementUp(1);
+            movementsManager.AddMovementDown(1);
+            movementsManager.AddMovementRight(1);
+            movementsManager.AddMovementUp(3);
+            movementsManager.AddMovementLeft(7);
+            movementsManager.AddMovementDown(3);
+            movementsManager.AddMovementLeft(3);
+            movementsManager.AddMovementUp(4);
+
+            movementsManager.AddMovementLeft(12);
+            movementsManager.AddMovementRight(11);
+            movementsManager.AddMovementUp(2);
+
 
             yield return TestUtilities.RunTest("World8Level3", movementsManager, GameState.Victory, 10.0f);
         }
