43710c599f13efffacd5cf419db2b21291bf8392
AlbertoNasarre-creator <albertonasarrecancer@gmail.com>
2025-03-26 15:20:56 +0100

    fix test

--- Assets/Tests/MovementsManager.cs
+++ Assets/Tests/MovementsManager.cs
@@ -40,6 +40,22 @@ public class MovementsManager
         if (buttonspressed[i] == ButtonPresed.CHANGEPLAYER)
         {
             Player.changePlayerTypeToMove();
+
+            GameObject SplungesTypesPanel = MapLoader.Instance.SplungesTypesPanel;
+
+            Transform splunges = SplungesTypesPanel.transform.GetChild(0); // Obtiene "Splunges"
+            GameObject splunje1 = splunges.GetChild(0).gameObject;
+            GameObject splunje2 = splunges.GetChild(1).gameObject;
+            GameObject splunje3 = splunges.GetChild(2).gameObject;
+
+            Canvas canvasHija = splunje1.transform.GetChild(0).gameObject.GetComponent<Canvas>();
+            canvasHija.overrideSorting = true;
+
+            splunje1.transform.GetChild(0).gameObject.SetActive(Player.currentAllowedType == Player.PlayerType.Type1);
+            splunje2.transform.GetChild(0).gameObject.SetActive(Player.currentAllowedType == Player.PlayerType.Type2);
+            splunje3.transform.GetChild(0).gameObject.SetActive(Player.currentAllowedType == Player.PlayerType.Type3);
+
+            canvasHija.overrideSorting = true;
         }
     }
 
